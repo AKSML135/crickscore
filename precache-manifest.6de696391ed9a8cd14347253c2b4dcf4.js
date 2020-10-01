beb4f8842058d8a8830662ee99a94622a2541e55
Savior135 <srivastava.amankumar03@gmail.com>
2020-10-01 13:37:33 +0530

    Updates

---
+++ precache-manifest.6de696391ed9a8cd14347253c2b4dcf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44c45b8cc8e147047c4cd702cf4ae73",
+    "url": "/crickscore/index.html"
+  },
+  {
+    "revision": "27ae5830cb4c71ee4c23",
+    "url": "/crickscore/static/css/main.95ecd92c.chunk.css"
+  },
+  {
+    "revision": "7b081f95e85eff3a3414",
+    "url": "/crickscore/static/js/2.6fd2559f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/crickscore/static/js/2.6fd2559f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ae5830cb4c71ee4c23",
+    "url": "/crickscore/static/js/main.eceb564c.chunk.js"
+  },
+  {
+    "revision": "95ed1838f25ec1bfaadb",
+    "url": "/crickscore/static/js/runtime-main.1ba1b98a.js"
+  },
+  {
+    "revision": "27b23a52df0c85503b9fe1b36595cb5a",
+    "url": "/crickscore/static/media/vs.27b23a52.png"
+  }
+]);
